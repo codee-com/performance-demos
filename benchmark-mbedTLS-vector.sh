5fe113dc10d640e353033eb2fccc01180160807f
Sergio Vazquez <sergio.vazquez@appentra.com>
2022-08-30 13:57:17 +0200

    Modified benchmark-mbedtls script to print a comparison table

--- benchmark-mbedTLS-vector.sh
+++ benchmark-mbedTLS-vector.sh
@@ -1,4 +1,5 @@
 #!/bin/bash -e
+export LC_NUMERIC="en_US.UTF-8"
 
 # Check that all required commands are available
 for cmd in git cmake ninja printf pwreport pwdirectives; do
@@ -29,18 +30,23 @@ printf "##################################################\n"
 printf "Wellcome to Codee's interactive demo with MBedTLS\n"
 printf "##################################################\n"
 printf "Seven steps:\n"
-printf "  1. Generate the compile_commads.json and build the neccesary files for the analysis\n"
-printf "  2. Codee's screening report for the whole suite\n"
-printf "  3. Codee's screening report for the most important algorightms\n"
-printf "  4. Vectorize the code with Codee's pwdirectives tool\n"
-printf "  5. Build the vectorized version\n"
-printf "  6. Verify the correctness of the vectorized version\n"
-printf "  7. Verify the speedup of the vectorized version\n"
+printf "  1. Build original MBbedTLS code\n"
+printf "  2. Vectorize the code with Codee's pwdirectives tool\n"
+printf "  3. Build the vectorized version\n"
+printf "  4. Verify the correctness\n"
+printf "  5. Verify the speedup\n"
 
 read -p "Press enter to start"
+
 printf "\n"
+printf "##################################################\n"
+printf "0/5. Getting the MBedTLS code ...\n"
+printf "##################################################\n"
+
+tSource0=$(date +%s%3N)
 git submodule update --init -- "MbedTLS/v3.1.0"
 cd "MbedTLS/v3.1.0/"
+tSource1=$(date +%s%3N)
 
 #===============================================================================
 
@@ -54,8 +60,10 @@ printf "\nDone.\n"
 #===============================================================================
 
 printf "##################################################\n"
-printf "1/7. Generating Compile_commands.json and building the neccesary files for the analysis ...\n"
+printf "1/5. Building original MBedTLS code ...\n"
 printf "##################################################\n"
+
+tBuild0=$(date +%s%3N)
 
 rm -rf build
 cmake \
@@ -69,33 +77,13 @@ cmake \
 
 cmake --build build
 
+tBuild1=$(date +%s%3N)
+
 #===============================================================================
 printf "\nStep 1 done.\n"
 printf "=============================================================\n\n"
 printf "##################################################\n"
-printf "2/7. Generating the Codee's Screening Report for the whole suite ...\n"
-printf "##################################################\n"
-read -p "Press enter to continue"
-printf "\n"
-
-pwreport --screening --level 2 --config build/compile_commands.json --show-progress --brief
-
-#===============================================================================
-printf "\nStep 2 done.\n"
-printf "=============================================================\n\n"
-printf "##################################################\n"
-printf "3/7. Generating the Codee's Screening Report for the most important algorithms ...\n"
-printf "##################################################\n"
-read -p "Press enter to continue"
-printf "\n"
-
-pwreport --screening --level 3 --config build/compile_commands.json --show-progress --brief library/aes.c library/cmac.c
-
-#===============================================================================
-printf "\nStep 3 done.\n"
-printf "=============================================================\n\n"
-printf "##################################################\n"
-printf "4/7. Vectorizing the code with Codee's pwdirectives tool ...\n"
+printf "2/5. Vectorizing the code with Codee's pwdirectives tool ...\n"
 printf "##################################################\n"
 read -p "Press enter to continue"
 printf "\n"
@@ -104,38 +92,29 @@ printf "\n++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
 printf "aes_xts algorithm\n"
 printf "++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++\n"
 
+tBuild2=$(date +%s%3N)
+
 pwdirectives --auto --simd omp --in-place --config build/compile_commands.json library/aes.c:mbedtls_aes_crypt_xts
-printf "\nDiff between the original and the vectorized versions:\n"
-(
-  set +e
-  git --no-pager diff library/aes.c
-  exit 0
-)
-printf "\n"
 
 printf "\n++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++\n"
 printf "cmac algorithm\n"
 printf "++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++\n"
 
 pwdirectives --auto --simd omp --in-place --config build/compile_commands.json library/cmac.c:cmac_xor_block
-printf "\nDiff between the original and the vectorized versions:\n"
-(
-  set +e
-  git --no-pager diff library/cmac.c
-  exit 0
-)
-printf "\n"
+
+
+tBuild3=$(date +%s%3N)
 
 #===============================================================================
-printf "\nStep 4 done.\n"
+printf "\nStep 2 done.\n"
 printf "=============================================================\n\n"
 printf "##################################################\n"
-printf "5/7. Building the vectorized version ...\n"
+printf "3/5. Building the vectorized version ...\n"
 printf "##################################################\n"
 read -p "Press enter to continue"
 printf "\n"
 
-rm -rf buildVec
+tBuild4=$(date +%s%3N)
 
 cmake \
 -DENABLE_TESTING=On \
@@ -150,14 +129,34 @@ cmake \
 
 cmake --build buildVec
 
+tBuild5=$(date +%s%3N)
+
 #===============================================================================
-printf "\nStep 5 done.\n"
+printf "\nStep 3 done.\n"
 printf "=============================================================\n\n"
 printf "##################################################\n"
-printf "6/7. Verifying the correctness of the vectorized version ...\n"
+printf "4/5. Verifying the correctness ...\n"
 printf "##################################################\n"
 read -p "Press enter to continue"
 printf "\n"
+
+tBuild6=$(date +%s%3N)
+
+cd "build/tests/"
+
+printf "\n++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++\n"
+printf "aes_xts algorithm\n"
+printf "++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++\n"
+./test_suite_aes.xts
+
+printf "\n++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++\n"
+printf "cmac algorithm\n"
+printf "++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++\n"
+./test_suite_cmac
+
+cd ../..
+
+tBuild7=$(date +%s%3N)
 
 cd "buildVec/tests/"
 
@@ -171,35 +170,56 @@ printf "cmac algorithm\n"
 printf "++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++\n"
 ./test_suite_cmac
 
+cd ../..
+tBuild8=$(date +%s%3N)
+
 #===============================================================================
 
-printf "\nStep 6 done.\n"
+printf "\nStep 4 done.\n"
 printf "=============================================================\n\n"
 printf "##################################################\n"
-printf "7/7. Verifying the speedup of the vectorized version ...\n"
+printf "5/5. Verifying the speedup ...\n"
 printf "##################################################\n"
 read -p "Press enter to continue"
 printf "\n"
-cd ../..
 
+tDeploy0=$(date +%s%3N)
 printf "\n++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++\n"
 printf "aes_xts algorithm\n"
 printf "++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++\n"
 build/programs/test/benchmark aes_xts
-printf "original done\n"
-printf "************************************************************\n"
-buildVec/programs/test/benchmark aes_xts
-printf "vectorized done\n"
-
 printf "\n++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++\n"
 printf "cmac algorithm\n"
 printf "++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++\n"
 build/programs/test/benchmark aes_cmac
 printf "original done\n"
-printf "************************************************************\n"
+
+tDeploy1=$(date +%s%3N)
+printf "\n++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++\n"
+printf "aes_xts algorithm\n"
+printf "++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++\n"
+buildVec/programs/test/benchmark aes_xts
+printf "\n++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++\n"
+printf "cmac algorithm\n"
+printf "++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++\n"
 buildVec/programs/test/benchmark aes_cmac
 printf "vectorized done\n"
-
+tDeploy2=$(date +%s%3N)
 #===============================================================================
 
-printf "\nDone.\n"
+tSource=$(bc -l <<< "($tSource1 - $tSource0) /1000")
+tBuildO=$(bc -l <<< "($tBuild1 - $tBuild0) /1000")
+tCodee=$(bc -l <<< "($tBuild3 - $tBuild2) /1000")
+tBuildC=$(bc -l <<< "($tBuild5 - $tBuild4) /1000")
+tTestO=$(bc -l <<< "($tBuild7 - $tBuild6) /1000")
+tTestC=$(bc -l <<< "($tBuild8 - $tBuild7) /1000")
+tDeployO=$(bc -l <<< "($tDeploy1 - $tDeploy0) /1000")
+tDeployC=$(bc -l <<< "($tDeploy2 - $tDeploy1) /1000")
+
+printf "\nStep               \tWithout Codee\tWith Codee\n"
+printf "=====================\t=============\t==========\n"
+printf "CI: SS: Clone        \t%.3f s\t\t%.3f s\n" $tSource $tSource
+printf "CI: BS: pwdirectives \t--- s\t\t%.3f s\n" $tCodee
+printf "CI: BS: make all     \t%.3f s\t\t%.3f s\n" $tBuildO $tBuildC
+printf "CI: BS: Test aes/cmac\t%.3f s\t\t%.3f s\n" $tTestO $tTestC
+printf "CI: DS: Benchmark    \t%.3f s\t\t%.3f s\n" $tDeployO $tDeployC
